fbab6765a491cebc6234f8c516e7a3ea37482b4d
RicardoBatel <112258124+RicardoBatel@users.noreply.github.com>
2022-10-17 18:49:36 -0300

    Tamanhos randômicos

--- jogo.js
+++ jogo.js
@@ -12,22 +12,40 @@ function ajustaTamanhoPalcoJogo() {
 ajustaTamanhoPalcoJogo()
 
 function posicaoRandomica() {
-var posicaox = Math.floor(Math.random() * largura) - 90
-var posicaoy = Math.floor(Math.random() * altura) - 90
+	var posicaox = Math.floor(Math.random() * largura) - 90
+	var posicaoy = Math.floor(Math.random() * altura) - 90
 
-posicaox = posicaox < 0 ? 0 : posicaox
-posicaoy = posicaoy < 0 ? 0 : posicaoy
+	posicaox = posicaox < 0 ? 0 : posicaox
+	posicaoy = posicaoy < 0 ? 0 : posicaoy
 
-console.log(posicaox, posicaoy)
+	console.log(posicaox, posicaoy)
 
-//criar o elemento html
+	//criar o elemento html
 
-var mosquito = document.createElement('img')
-mosquito.src = 'imagens/mosquito.png'
-mosquito.className = 'mosquito1'
-mosquito.style.left = posicaox + 'px'
-mosquito.style.top = posicaoy + 'px'
-mosquito.style.position = 'absolute'
+	var mosquito = document.createElement('img')
+	mosquito.src = 'imagens/mosquito.png'
+	mosquito.className = tamanhoAleatorio()
+	mosquito.style.left = posicaox + 'px'
+	mosquito.style.top = posicaoy + 'px'
+	mosquito.style.position = 'absolute'
 
-document.body.appendChild(mosquito)
+	document.body.appendChild(mosquito)
+
+	tamanhoAleatorio()
+
+}
+
+function tamanhoAleatorio() {
+	var classe = Math.floor(Math.random() * 3)
+	
+	switch(classe) {
+		case 0:
+			return 'mosquito1'
+
+		case 1:
+			return 'mosquito2'
+
+		case 2:
+			return 'mosquito3'
+	}
 }
