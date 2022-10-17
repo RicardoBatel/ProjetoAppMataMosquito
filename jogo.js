98d1de16dad323a97e174ddf0c7a26b1d045ce63
RicardoBatel <112258124+RicardoBatel@users.noreply.github.com>
2022-10-17 16:55:26 -0300

    Criando posições randômicas

--- jogo.js
+++ jogo.js
@@ -9,5 +9,25 @@ function ajustaTamanhoPalcoJogo() {
 	console.log(altura, largura)
 }
 
-ajustaTamanhoPalcoJogo()	
+ajustaTamanhoPalcoJogo()
 
+function posicaoRandomica() {
+var posicaox = Math.floor(Math.random() * largura) - 90
+var posicaoy = Math.floor(Math.random() * altura) - 90
+
+posicaox = posicaox < 0 ? 0 : posicaox
+posicaoy = posicaoy < 0 ? 0 : posicaoy
+
+console.log(posicaox, posicaoy)
+
+//criar o elemento html
+
+var mosquito = document.createElement('img')
+mosquito.src = 'imagens/mosquito.png'
+mosquito.className = 'mosquito1'
+mosquito.style.left = posicaox + 'px'
+mosquito.style.top = posicaoy + 'px'
+mosquito.style.position = 'absolute'
+
+document.body.appendChild(mosquito)
+}
